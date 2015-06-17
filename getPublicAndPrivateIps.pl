c7839e9b44c375fb6def0d55282eea62a1f68152
Timothy Humphrey <timothy.humphrey@lexisnexis.com>
2015-06-17 17:01:08 -0400

    Initial load of repository, EasyFastHPCCoAWS
    Signed-off-by: Timothy Humphrey <timothy.humphrey@lexisnexis.com>

---
+++ getPublicAndPrivateIps.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl
+=pod
+  Note: The following 2 examples assume you are in the directory, ~/BestHoA
+ 
+  Use following if instances are ubuntu
+  perl getPublicAndPrivateIPsOfAllInstances.pl instance_ids.txt instance_files ec2-user us-west-2
+=cut
+ 
+require "/home/ec2-user/getConfigurationFile.pl";
+
+ print "Entering getPublicAndPrivateIps.pl\n";
+ 
+ open(IN, $instance_ids) || die "Can't open for input: \"$instance_ids\"\n";
+ 
+ my $private_ips_file="/home/ec2-user/private_ips.txt";
+ my $public_ips_file="/home/ec2-user/public_ips.txt";
+ open(OUT1,">$private_ips_file") || die "Can't open for output: \"$private_ips_file\"\n";
+ open(OUT2,">$public_ips_file") || die "Can't open for output: \"$public_ips_file\"\n";
+ 
+ while(<IN>){
+    next if /^#/ || /^\s*$/;
+    chomp;
+    print "DEBUG: Input from <IN> is \"$_\"\n";
+    my $instance_id=$_;
+    print "DEBUG: instance_id=\"$instance_id\"\n";
+    
+    print "AWS COMMAND IS: aws ec2 describe-instances --instance-ids $instance_id --region $region\n\n";
+    local $_=`aws ec2 describe-instances --instance-ids $instance_id --region $region`;
+    print "DEBUG: Length of AWS's output is: ",length($_),"\n";
+    my $public_ip = $1 if /\"PublicIpAddress\": \"(\d+\.\d+\.\d+\.\d+)\"/;
+    my $private_ip = $1 if /\"PrivateIpAddress\": \"(\d+\.\d+\.\d+\.\d+)\"/;
+    print "DEBUG: public_ip=\"$public_ip\"\n";
+    print "DEBUG: private_ip=\"$private_ip\"\n";
+    print OUT1 "$private_ip\n";
+    print OUT2 "$public_ip\n";
+ }
+ 
+ close(OUT1);
+ close(OUT2);
+ print "Outputting $public_ips_file\n";
+ print "Outputting $private_ips_file\n";
+ 
---
+++ initial_configureHPCC.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -e
+
+. /home/ec2-user/cfg_BestHPCC.sh
+
+# Change name of $created_environment_file so it is prefixed with "intial_"
+basename=$(basename "$created_environment_file")
+path_and_basename="${created_environment_file%.*}"
+path=`echo $path_and_basename|sed "s/^\(..*\/\).*$/\1/"`
+created_environment_file="${path}initial_$basename"
+
+slavesPerNode=1
+
+set2falseRoxieMultiCaseEnabled=''
+if [ $roxienodes -gt 0 ]
+then
+  set2falseRoxieMultiCaseEnabled=' -override roxie,@roxieMultiCastEnabled,false'
+fi
+
+envgen=/opt/HPCCSystems/sbin/envgen;
+
+# Make new environment.xml file for newly configured HPCC System.
+echo "$envgen -env $created_environment_file $set2falseRoxieMultiCaseEnabled -override esp,@method,htpasswd -override thor,@replicateAsync,true -override thor,@replicateOutputs,true -ipfile $private_ips -supportnodes $supportnodes -thornodes $non_support_instances -roxienodes $roxienodes -slavesPerNode $slavesPerNode -roxieondemand 1"
+
+$envgen  -env $created_environment_file $set2falseRoxieMultiCaseEnabled -override esp,@method,htpasswd -override thor,@replicateAsync,true -override thor,@replicateOutputs,true -ipfile $private_ips -supportnodes $supportnodes -thornodes $non_support_instances -roxienodes $roxienodes  -slavesPerNode $slavesPerNode -roxieondemand 1
+echo "Completed $envgen"
+
+# Copy the newly created environment file  to /etc/HPCCSystems on all nodes of the THOR
+out_environment_file=/etc/HPCCSystems/environment.xml
+master_ip=`head -1 /home/ec2-user/public_ips.txt`
+echo "ssh -o StrictHostKeyChecking=no -t -t -i $pem ec2-user@$master_ip \"sudo /opt/HPCCSystems/sbin/hpcc-push.sh -s $created_environment_file -t $out_environment_file\""
+ssh -o StrictHostKeyChecking=no -t -t -i $pem ec2-user@$master_ip "sudo /opt/HPCCSystems/sbin/hpcc-push.sh -s $created_environment_file -t $out_environment_file"
+echo "Completed hpcc-push.sh"
