d7a8afa977962ac9510fd25e4db0f5a9f3bacf9f
Timothy L Humphrey <timothy.humphrey@lexisnexis.com>
2015-07-22 16:42:37 -0400

    Update updateSystemFilesOnAllInstances.pl

--- updateSystemFilesOnAllInstances.pl
+++ updateSystemFilesOnAllInstances.pl
@@ -1,14 +1,7 @@
 #!/usr/bin/perl
-=pod
- Note: The following 2 examples assume you are in the directory, $share/naveens_data
- Use the following if instances are Amazon linux AMIs
-
- perl updateSystemFilesOnAllInstances.pl public_ips.txt ec2-user tlh_keys_us_west_2.pem
-=cut
-
 require "/home/ec2-user/getConfigurationFile.pl";
  
-open(IN, $public_ips) || die "Can't open for input: \"$public_ips\"\n";
+open(IN, $private_ips) || die "Can't open for input: \"$private_ips\"\n";
  
 while(<IN>){
    next if /^#/;
